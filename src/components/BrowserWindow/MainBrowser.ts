5b391bcf64d6826e2ca7c6b5201b507d4c2e4541
diospiroverde <79201496+diospiroverde@users.noreply.github.com>
2021-04-13 04:32:04 +0100

    Update MainBrowser.ts

--- src/components/BrowserWindow/MainBrowser.ts
+++ src/components/BrowserWindow/MainBrowser.ts
@@ -398,9 +398,7 @@ export class MainBrowser extends EventEmitter {
         //content eventsdark
         this.win.webContents.on('did-finish-load', async () => {
             await this.ScriptLoad();
-
-            if(Settings.hideNotifications.value)           
-                        
+                                          
             this.SendConfigs();
             if(Settings.batteryWarning.value)
                 this.win.webContents.executeJavaScript('{var lastTime = false; var interval = setInterval(() => {if((document.documentElement.textContent || document.documentElement.innerText).indexOf(\'Phone battery low\') > -1){ if(lastTime == false) {const { ipcRenderer:ipcRendererNotification } = require(\'electron\');ipcRendererNotification.send(\'battery-low\');lastTime = true}} else {lastTime = false }},3000)}');
