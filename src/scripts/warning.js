d6577272b7271150c1d48d482f62496dff6fff86
diospiroverde <79201496+diospiroverde@users.noreply.github.com>
2021-11-07 01:11:29 +0000

    Add files via upload

--- src/scripts/warning.js
+++ src/scripts/warning.js
@@ -1,5 +1,5 @@
 var lastTime = false;
-var interval = setInterval(() => {if((document.documentElement.textContent || document.documentElement.innerText).indexOf('Phone battery low') > -1){
+var interval = setInterval(() => {if(document.body.innerHTML.search('alert-battery') != -1){
 if(lastTime == false) {const { ipcRenderer:ipcRendererNotification } = require('electron');
 ipcRendererNotification.send('battery-low');
 lastTime = true}} else {lastTime = false }},3000)
